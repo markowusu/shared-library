87cb1b71811539adbee17287de3ae353ff330e0f
Mark Owusu <markowusuayim@gmail.com>
2023-01-28 21:27:29 +0100

    first commit

---
+++ shared-library/vars/filterlogs.groovy
@@ -0,0 +1,18 @@
+#!/usr/bin/evn groovy
+
+// creating this function to filter the logs based on the keyword passed in.
+
+import org.apache.commons.lang.StringUtils
+
+def filterBuildLogs(String filter_string, int occurences,int log_num = 1000){
+    def logs  = currentBuild.rawBuild.getLog(log_num).join('\n')
+
+    int count = StringUtils.countMatches(logs, filter_string);
+
+    if(count > occurences -1 ){
+        currentBuild.result = 'UNSTABLE' 
+    }
+}
+
+
+
