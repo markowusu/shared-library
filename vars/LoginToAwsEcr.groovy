c5356a32788bf9c76d2c45a2d9b5732ed0952692
Mark Owusu <markowusuayim@gmail.com>
2023-02-01 22:50:27 +0100

    add: LoginShared Lib

--- vars/LoginToAwsEcr.groovy
+++ vars/LoginToAwsEcr.groovy
@@ -1,9 +1,12 @@
 #!/usr/bin/env groovy
 def call(Map args) {
+
     def awsCredentialsId = args.credentialsId
+
     def awsRegion = args.awsRegion
 
     withCredentials([aws(credentialsId: awsCredentialsId, region: awsRegion)]) {
+        
         sh "aws ecr get-login-password | docker login --username AWS --password-stdin ${args.awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com"
     }
 }
