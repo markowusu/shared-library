47a50dd02ddd3f31adc6461d2ef8b4bf4b5df939
Mark Owusu <markowusuayim@gmail.com>
2023-02-01 20:12:09 +0100

    feat: added aws ecr & docker steps to the pipeline

--- vars/version.groovy
+++ vars/version.groovy
@@ -1,5 +1,9 @@
 #!/urs/bin/env groovy 
 
 def call(String packageName){
-    sh 'pip3 list | findstr ${packageName}'
+    sh '''
+    pip3 list | findstr ${packageName}
+    aws --version 
+    
+    '''
 }
