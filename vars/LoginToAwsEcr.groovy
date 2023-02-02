f70cf35065403b631dfb1f55a7c498d6e6971ff0
Mark Owusu <markowusuayim@gmail.com>
2023-02-03 00:20:15 +0100

    fix: argument for awsAccountId

--- vars/LoginToAwsEcr.groovy
+++ vars/LoginToAwsEcr.groovy
@@ -4,12 +4,15 @@
 def call(Map args) {
 
     def awsCredentialsId = args.credentialsId
-    def awsAccountId = args.awsAccountId
+
     def awsRegion = args.awsRegion
+    
+    def awsAccountId = args.awsAccountId
 
     withCredentials([aws(credentialsId: awsCredentialsId, region: awsRegion)]) {
 
-        sh "docker login --username AWS -p $(aws ecr get-login-password --region ${awsRegion}) ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com"
-       
+        sh "aws ecr get-login-password | docker login --username AWS --password-stdin ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com"
+        // sh "docker login --username AWS -p $(aws ecr get-login-password --region ${awsRegion}) ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com"
+
     }
 }
