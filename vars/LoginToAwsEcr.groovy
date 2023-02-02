5f8c1812add0451759ca18c688530b5d915ce79b
Mark Owusu <markowusuayim@gmail.com>
2023-02-03 00:30:32 +0100

    fix: adding region to the sh cmd

--- vars/LoginToAwsEcr.groovy
+++ vars/LoginToAwsEcr.groovy
@@ -6,13 +6,13 @@ def call(Map args) {
     def awsCredentialsId = args.credentialsId
 
     def awsRegion = args.awsRegion
-    
+
     def awsAccountId = args.awsAccountId
 
     withCredentials([aws(credentialsId: awsCredentialsId, region: awsRegion)]) {
 
-        sh "aws ecr get-login-password | docker login --username AWS --password-stdin ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com"
-        // sh "docker login --username AWS -p $(aws ecr get-login-password --region ${awsRegion}) ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com"
+        // sh "aws ecr get-login-password | docker login --username AWS --password-stdin ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com"
+        sh "docker login --username AWS -p $(aws ecr get-login-password --region ${awsRegion}) ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com"
 
     }
 }
