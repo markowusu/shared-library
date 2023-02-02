269b63b1e17c17c38ee1cd87f8609789f9437869
Mark Owusu <markowusuayim@gmail.com>
2023-02-03 00:13:51 +0100

    fix: removed args in the string interpolation of awsAccountId

--- vars/LoginToAwsEcr.groovy
+++ vars/LoginToAwsEcr.groovy
@@ -4,12 +4,12 @@
 def call(Map args) {
 
     def awsCredentialsId = args.credentialsId
-
+    def awsAccountId = args.awsAccountId
     def awsRegion = args.awsRegion
 
     withCredentials([aws(credentialsId: awsCredentialsId, region: awsRegion)]) {
 
-        sh "docker login --username AWS -p $(aws ecr get-login-password --region ${awsRegion}) ${args.awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com"
+        sh "docker login --username AWS -p $(aws ecr get-login-password --region ${awsRegion}) ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com"
        
     }
 }
