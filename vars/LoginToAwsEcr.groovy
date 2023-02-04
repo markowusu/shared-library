539bbe54b139dff7dfccba2de05183bae3e2e5f6
Mark Owusu <markowusuayim@gmail.com>
2023-02-04 08:54:19 +0100

    add: aws get-caller-identity

--- vars/LoginToAwsEcr.groovy
+++ vars/LoginToAwsEcr.groovy
@@ -9,18 +9,20 @@ def call(Map args) {
 
     def awsAccountId = args.awsAccountId
 
-      withCredentials([[$class: 'AmazonWebServicesCredentialsBinding',credentialsId: awsCredentialsId]]) {
-   container('aws-cli') {
-      sh('env')
-      sh('aws sts get-caller-identity')
+//       withCredentials([[$class: 'AmazonWebServicesCredentialsBinding',credentialsId: awsCredentialsId]]) {
+//    container('aws-cli') {
+//       sh('env')
+//       sh('aws sts get-caller-identity')
+//     }
+// }
+
+    withCredentials([aws(credentialsId: awsCredentialsId, region: awsRegion)]) {
+
+        // sh "aws ecr get-login-password | docker login --username AWS --password-stdin ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com"
+     sh 'echo ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com'
+      sh 'env'
+      sh 'aws sts get-caller-identity'
+        sh 'docker login --username AWS --password $(aws ecr get-login-password --region ${awsRegion}) ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com'
+
     }
-}
-
-    // withCredentials([aws(credentialsId: awsCredentialsId, region: awsRegion)]) {
-
-    //     // sh "aws ecr get-login-password | docker login --username AWS --password-stdin ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com"
-    //  sh 'echo ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com'
-    //     sh 'docker login --username AWS --password $(aws ecr get-login-password --region ${awsRegion}) ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com'
-
-    // }
 }
