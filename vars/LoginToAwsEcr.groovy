2a4c85dc44f99df4f7bb26a71a079979e780a419
Mark Owusu <markowusuayim@gmail.com>
2023-02-05 20:53:14 +0100

    fix: added a new ecr address for the repo

--- vars/LoginToAwsEcr.groovy
+++ vars/LoginToAwsEcr.groovy
@@ -13,9 +13,11 @@ def call(Map args) {
 
         // sh "aws ecr get-login-password | docker login --username AWS --password-stdin ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com"
         echo '${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com'
-        
-        sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 001526952227.dkr.ecr.us-east-1.amazonaws.com'
+
+        // sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 001526952227.dkr.ecr.us-east-1.amazonaws.com'
         // sh 'docker login --username AWS -p $(aws ecr get-login-password --region ${awsRegion}) ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com'
+        sh 'docker login --username AWS --password-stdin $(aws ecr get-login-password --region us-east-1 ) 001526952227.dkr.ecr.us-east-1.amazonaws.com'
+
 
     }
 }
