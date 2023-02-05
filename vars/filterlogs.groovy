e53735b2f7aafd6bb823efb4519534b3c2d45b45
Mark Owusu <markowusuayim@gmail.com>
2023-02-06 00:49:49 +0100

    feat: added  a stage to push changes based on tags to github

--- vars/filterlogs.groovy
+++ vars/filterlogs.groovy
@@ -8,10 +8,16 @@ def call(String filter_string, int occurences,int log_num = 1000){
     def logs  = currentBuild.rawBuild.getLog(log_num).join('\n')
 
     int count = StringUtils.countMatches(logs, filter_string);
+    
+    if (filter_string == 'ERROR' && count == 1 ){
+        currentBuild.result = "FAILURE"
+    }
+
 
     if(count > occurences -1 ){
         currentBuild.result = 'UNSTABLE' 
     }
+     
 }
 
 
