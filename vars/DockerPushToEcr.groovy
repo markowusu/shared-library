1dfd9ed60a384590465141c828f138a1999ee5a3
Mark Owusu <markowusuayim@gmail.com>
2023-02-11 18:30:53 +0100

    fix: changing the single quote to a double to enable string interpolation

--- vars/DockerPushToEcr.groovy
+++ vars/DockerPushToEcr.groovy
@@ -4,11 +4,11 @@ def call(String imageTag, String dockerImageName, String repository){
     
     def buildID = env.BUILD_ID
     sh'echo tagging the built image to be pushed to the new repo on aws '
-                sh 'docker tag ${dockerImageName}:${imageTag} ${repository}:${buildID}'
+                sh "docker tag ${dockerImageName}:${imageTag} ${repository}:${buildID}"
             
                 
                 sh 'echo pushng new image to the ECR repo on aws'
-                sh 'docker push ${repository}:${buildID}'
+                sh "docker push ${repository}:${buildID}"
 }
 
 
