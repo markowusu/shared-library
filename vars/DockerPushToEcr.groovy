5fc2e88ad1e9dcce25b8d7af347d6005fc887c05
Mark Owusu <markowusuayim@gmail.com>
2023-02-11 18:10:32 +0100

    fix: added a global variable for the docker image name

--- vars/DockerPushToEcr.groovy
+++ vars/DockerPushToEcr.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
-def call(String imageTag, String repository){
+def call(String imageTag, String dockerImageName, String repository){
     
     def buildID = env.BUILD_ID
     sh'echo tagging the built image to be pushed to the new repo on aws '
-                sh 'docker tag ${imageTag} ${repository}:${buildID}'
+                sh 'docker tag ${dockerImageName}:${imageTag} ${repository}:${buildID}'
             
                 
                 sh 'echo pushng new image to the ECR repo on aws'
