38fe77774c9257286ac1145053008f310f3b908e
Mark Owusu <markowusuayim@gmail.com>
2023-02-11 17:49:18 +0100

    fix: removed interpolation for the shared lib. LoginToEcr

--- vars/LoginToAwsEcr.groovy
+++ vars/LoginToAwsEcr.groovy
@@ -12,11 +12,12 @@ def call(Map args) {
     withCredentials([aws(credentialsId: awsCredentialsId, region: awsRegion)]) {
 
         // sh "aws ecr get-login-password | docker login --username AWS --password-stdin ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com"
-        // echo '${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com'
+        echo '${env}'
+        println '${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com'
 
         // sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 001526952227.dkr.ecr.us-east-1.amazonaws.com'
         // sh 'docker login --username AWS -p $(aws ecr get-login-password --region ${awsRegion}) ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com'
-        sh 'docker login --username AWS -p $(aws ecr get-login-password --region ${awsRegion} ) ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com'
+        sh 'docker login --username AWS -p $(aws ecr get-login-password --region us-east-1 ) 001526952227.dkr.ecr.us-east-1.amazonaws.com'
 
 
     }
